9f0ef30d0ceb0b22e5b7fe711b03832f8c524a6e
ZhangKuo <emtric.cn@gamil.com>
2023-12-16 17:01:15 +0800

    Initial commit

---
+++ test/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time : 2023/12/16 16:55
+# @Author : ZhangKuo
---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time : 2023/12/16 16:59
+# @Author : ZhangKuo
